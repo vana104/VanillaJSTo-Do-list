90cc642415c2c152d8edae8c66fbcb513264cbd4
vana104 <avheerden7@gmail.com>
2017-08-11 20:14:22 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//fuction takes input from user and prints it out
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+//when input is not given tell user, else is says to user You must write something!
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+}
